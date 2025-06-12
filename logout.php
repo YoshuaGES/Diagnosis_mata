b4f6d22c3c7bc892b3c6f44ca83367401aa90c31
Yoshua <auhsoywatchandlearn@gmail.com>
2025-06-12 18:49:36 +0700

    logout sesion

--- logout.php
+++ logout.php
@@ -1,10 +1,15 @@
 <?php 
 // mengaktifkan session
 session_start();
- 
+
+session_unset(); // menghapus semua variabel session
+
 // menghapus semua session
 session_destroy();
  
 // mengalihkan halaman sambil mengirim pesan logout
 header("Location:login.php");
+
+exit;
+
 ?>
